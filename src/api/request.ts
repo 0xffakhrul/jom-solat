8d0fc265da13a12e48bf8e8816073b8f9f62d4d8
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-09-09 18:24:51 +0800

    create a request to the api

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,35 +1,43 @@
 import axios from "axios";
+import zonesData from "../zones.json";
 
-export interface zone {
+export interface Zone {
   zone: string;
   state: string;
   area: string[];
 }
 
-export interface PrayerTime {
-  prayerTime: {
-    date: string;
-    fajr: string;
-    dhuhr: string;
-    asr: string;
-    maghrib: string;
-    isha: string;
-  }[];
+export interface PrayerTimeData {
+  hijri: string;
+  date: string;
+  day: string;
+  imsak: string;
+  fajr: string;
+  syuruk: string;
+  dhuhr: string;
+  asr: string;
+  maghrib: string;
+  isha: string;
 }
 
-export const fetchPrayerTimes = async (zone: string) => {
-  const response = await axios.get<PrayerTime>(
+export interface PrayerTimeResponse {
+  prayerTime: PrayerTimeData[];
+  status: string;
+  serverTime: string;
+  periodType: string;
+  lang: string;
+  zone: string;
+  bearing: string;
+}
+
+export const fetchPrayerTimes = async (zone: string): Promise<PrayerTimeData> => {
+  const response = await axios.get<PrayerTimeResponse>(
     `https://www.e-solat.gov.my/index.php?r=esolatApi/takwimsolat&period=today&zone=${zone}`
   );
 
-  const prayerTimes = response.data.prayerTime[0];
+  return response.data.prayerTime[0];
+};
 
-  return {
-    date: prayerTimes.date,
-    fajr: prayerTimes.fajr,
-    dhuhr: prayerTimes.dhuhr,
-    asr: prayerTimes.asr,
-    maghrib: prayerTimes.maghrib,
-    isha: prayerTimes.isha,
-  };
+export const getZones = (): Zone[] => {
+  return zonesData;
 };
